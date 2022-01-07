08fb27373301e7f10cbe8a8275915652bdbef31a
yann1294 <dev.yann12@gmail.com>
2022-01-07 15:25:54 +0000

    updated server

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
 
     # path for about view
 
+    path(route='about', view= views.about, name='about'),
+
     # path for contact us view
+    path(route='contact', view= views.contact, name='contact'),
 
     # path for registration
 
